29b1be2c17c4776280e0a9232da8f45b783c143d
Chaitanya Gumudala <chaitanyagumudala961@gmail.com>
2024-02-27 15:55:42 +0100

    added rviz command to launch file

--- differential_drive_robot/launch/robot_state_publisher.launch.py
+++ differential_drive_robot/launch/robot_state_publisher.launch.py
@@ -3,6 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 import xacro
 
@@ -15,6 +16,7 @@ def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('differential_drive_robot'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
+    rviz_config_file = os.path.join(pkg_path, 'config', 'view_robot.rviz')
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
@@ -23,6 +25,32 @@ def generate_launch_description():
         executable='robot_state_publisher',
         output='screen',
         parameters=[params]
+    )
+    gui_arg = DeclareLaunchArgument(
+        name='gui',
+        default_value='True'
+    )
+
+    show_gui = LaunchConfiguration('gui')
+    joint_state_publisher_node = Node(
+        condition=UnlessCondition(show_gui),
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    joint_state_publisher_gui_node = Node(
+        condition=IfCondition(show_gui),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        output='screen'
     )
 
 
@@ -33,5 +61,9 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        gui_arg,
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node
     ])
