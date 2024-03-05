fcf1f4d3be49cc21d310cdecf5936739cffbf4bf
Chaitanya Gumudala <chaitanyagumudala961@gmail.com>
2024-03-05 21:07:00 +0100

    fixed bugs and loaded chassis with wheels in gazebo and rviz

--- mobile_manipulator/launch/mobile_manipulator_simulation.launch.py
+++ mobile_manipulator/launch/mobile_manipulator_simulation.launch.py
@@ -19,9 +19,11 @@ def generate_launch_description():
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ),
+        # launch_arguments={'pause': 'true'}.items()
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
